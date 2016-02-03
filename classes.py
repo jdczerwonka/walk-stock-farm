22f03ed6035a1dce3109175f5c2dd82387e25da1
jdczerwonka <jonathan.czerwonka@gmail.com>
2016-02-03 14:45:01 -0600

    Chrystalized Model Interrelations
    Finally conceptually figured out fi vs fc vs g for avg wk AND cum

--- classes.py
+++ classes.py
@@ -155,23 +155,28 @@ class SalesModel:
         return ( self.carcass_df[ColumnStr].sum() * self.carcass_df["num"].sum() ) / self.carcass_df["num"].sum()
 
 class PigGrowthModel():
-    def __init__(self, awgModel = [0], awfcModel = [0], deathModel = [0], StartWeight = 0,
-                 awgAdjust = None, awfcAdjust = None, PriceCutoff = [0], WtCutoff = WT_CUTOFF_DEFAULT, WeekCutoff = None):
-        
+    def __init__(self, awgModel = [0], awfcModel = [0],
+                 awgAdjust = None, awfcAdjust = None, 
+                 PriceCutoff = [0], WtCutoff = WT_CUTOFF_DEFAULT, WeekCutoff = None,
+                 StartWeight = 12):
+
         self.start_weight = StartWeight
 
         self.awg = Model( polynomial(awgModel) )
         if awgAdjust is not None:
             self.shift_awg(awgAdjust[0], awgAdjust[1], awgAdjust[2])
 
+        self.set_g_total
+        self.set_g_cum
+
         self.awfc = Model( polynomial(awfcModel) )
         if awfcAdjust is not None:
             self.shift_awfc(awfcAdjust[0], awfcAdjust[1], awfcAdjust[2])
 
-        self.set_gain
         self.set_fc_cum
         self.set_awfi
-        self.set_awfi_cum
+        self.set_fi_total
+        self.set_fi_cum
 
         self.cutoff_price = PriceCutoff
         self.cutoff_wt = WtCutoff
@@ -180,24 +185,36 @@ class PigGrowthModel():
             self.calc_week_cutoff
 
         self.feed_cost = Model(lambda x: self.heaviside_combined(x))
-        self.feed_cost_total = Model(lambda x: self.feed_cost.model(x) * self.awfi.model(x))
+        self.aw_feed_cost = Model(lambda x: self.feed_cost.model(x) * self.awfi.model(x))
 
     def shift_awg(self, zero, lb, ub):
         self.awg = Model( self.awg.model * newton(lambda x: quad(self.awg.model * x, lb, ub)[0] - zero, 1))
 
     def shift_awfc(self, zero, lb, ub):
-        self.awfc = Model( polynomial( [1, newton(lambda x: ( self.awfc.integrate(lb, ub, ub - lb, x) ) - zero, 0)] ) )
+        # self.awfc = Model( polynomial( [self.awfc.model.coef[0], newton(lambda x: self.awfc.integrate(lb, ub, ub - lb, x) - zero, 0)] ) )
+        self.awfc = Model( polynomial( [self.awfc.model.coef[0], newton(lambda x: self.opt_fc(x, ub) - zero, 0)] ) )
+
+    def opt_fc(self, x, wk):
+        self.awfc.model.coef[1] = x
+        self.set_awfi
+        self.set_fi_cum
+        self.set_fc_cum
+        return self.fc_cum.model(wk)
 
     @property
-    def set_gain(self):
+    def set_g_total(self):
         p = self.awg.model.integ()
-        self.gain = Model( polynomial(p.coef) )
+        self.g_total = Model( polynomial(p.coef) )
+
+    @property 
+    def set_g_cum(self):
+        p = self.awg.model.integ()
+        m = polynomial(p.coef)
+        self.g_cum = Model(lambda x: m(x) / (x * 7) )
 
     @property
     def set_fc_cum(self):
-        p = self.awfc.model.integ()
-        m = polynomial(p.coef)
-        self.fc_cum = Model(lambda x: m(x) / x )
+        self.fc_cum = Model(lambda x: self.fi_cum.model(x) / self.g_cum.model(x))
 
     @property
     def set_awfi(self):
@@ -205,15 +222,21 @@ class PigGrowthModel():
         self.awfi = Model( polynomial(p.coef) )
 
     @property
-    def set_awfi_cum(self):
+    def set_fi_total(self):
         p = self.awfi.model.integ()
-        self.awfi_cum = Model( polynomial(self.awfi) )    
+        self.awfi_total = Model( polynomial(self.awfi) ) 
+
+    @property 
+    def set_fi_cum(self):
+        p = self.awfi.model.integ()
+        m = polynomial(p.coef)
+        self.fi_cum = Model(lambda x: m(x) / (x * 7) )
 
     @property
     def calc_week_cutoff(self):
         self.cutoff_week = []
         for wt in self.cutoff_wt:
-            self.cutoff_week.append(self.awg.calc_week(wt))
+            self.cutoff_week.append(self.awg.calc_week(wt - self.start_weight))
 
         self.cutoff_week.append(26)
 
@@ -228,8 +251,10 @@ class PigGrowthModel():
         return func 
 
 class BarnModel():
-    def __init__(self, DeathModel, PigModel, BarnSize = 2500, DeathLossPer = 3.25):
+    def __init__(self, DeathModel, PigModel, StartWeight = 12, BarnSize = 2500, DeathLossPer = 3.25):
+        
         self.death = Model( polynomial(DeathModel) )
+        self.start_weight = StartWeight
         self.barn_size = BarnSize
         self.death_loss = DeathLossPer
 
@@ -237,28 +262,80 @@ class BarnModel():
 
         self.pig = PigModel
 
-        self.awg = Model(lambda x: self.pig.awg.model(x) * self.alive_shift.model(x))
-        self.awfc = Model(lambda x: self.pig.awfc.model(x) * self.alive_shift.model(x))
-        self.awfi = Model(lambda x: self.pig.awfi.model(x) * self.alive_shift.model(x))
-        self.feed_cost_total = Model(lambda x: self.pig.feed_cost_total.model(x) * self.alive_shift.model(x))
+        self.set_awg
+        self.set_g_total
+        self.set_g_cum
+
+        self.set_awfi
+        self.set_fi_total
+        self.set_fi_cum
+
+        self.set_awfc
+        self.set_fc_cum
+
+        self.aw_feed_cost = Model(lambda x: self.pig.aw_feed_cost.model(x) * self.alive.model(x))
+
+    def calc_feed_cost_diff(self, lb, ub, data_type = "wk"):
+        if data_type == "wt":
+            lb = self.pig.awg.calc_week(lb - self.start_weight)
+            ub = self.pig.awg.calc_week(ub - self.start_weight)
+
+        return self.aw_feed_cost.integrate(lb, ub)
+
+    @property 
+    def set_awg(self):
+        p = self.pig.awg.model * self.alive.model
+        self.awg = Model( polynomial(p.coef) )
+
+    @property 
+    def set_g_total(self):
+        p = self.awg.model.integ()
+        self.g_total = Model( polynomial(p.coef) )
+
+    @property 
+    def set_g_cum(self):
+        p = self.awg.model.integ()
+        m = polynomial(p.coef)
+        self.g_cum = Model( lambda x: m(x) / (x * 7 * self.barn_size) )
+
+    @property 
+    def set_awfc(self):
+        p = self.pig.awfc.model * self.alive.model
+        self.awfc = Model( polynomial(p.coef) )
+
+    @property 
+    def set_fc_cum(self):
+        p = self.awfc.model.integ()
+        m = polynomial(p.coef)
+        self.fc_cum = Model( lambda x: m(x) / (x * self.barn_size) )
+
+    @property 
+    def set_awfi(self):
+        p = self.pig.awfi.model * self.alive.model
+        self.awfi = Model( polynomial(p.coef) )
+
+    @property 
+    def set_fi_total(self):
+        p = self.awfi.model.integ()
+        self.fi_total = Model( polynomial(p.coef) )
+
+    @property 
+    def set_fi_cum(self):
+        p = self.awfi.model.integ()
+        m = polynomial(p.coef)
+        self.fi_cum = Model( lambda x: m(x) / (x * 7 * self.barn_size) )
 
     @property
     def adjust_death(self):
         adjust = self.death.integrate(0, 26)
-        self.death.model.coef = self.death.model.coef * (self.death_loss / 100 * self.barn_size / adjust)
+        p = self.death.model * (self.death_loss / 100 * self.barn_size / adjust)
+        self.death = Model( polynomial(p.coef) )
         self.set_alive
-        self.shift_death
 
     @property
     def set_alive(self):
         p = self.barn_size - self.death.model.integ()
         self.alive = Model( polynomial(p.coef) )
-
-    @property
-    def shift_death(self):
-        roots = self.death.model.roots()
-        self.death_shift = Model(lambda x: self.death.model(x + roots[0]))
-        self.alive_shift = Model(lambda x: self.alive.model(x + roots[0]))
 
 class Model():
     def __init__(self, func):
--- test.py
+++ test.py
@@ -6,21 +6,27 @@ import numpy
 import matplotlib.pyplot as plt
 from scipy.integrate import quad
 
-w2fModel = [-203.950303, 288.798017, -81.388061, 10.101958, -0.623565, 0.018835, -0.000222]
-finModel = [-130.739421, 210.431209, -65.244076, 9.294793, -0.678313, 0.024900, -0.000365]
-awgModel = [0.43503557, 2.16250341, -0.09743488, 0.00122924]
-abilene = -0.87031545
-awfcModel = [1, 0.10728206]
-awgAdjust = [273, 0, 26]
-fccumAdjust = [2.57, 0, 26]
+polynomial = numpy.polynomial.polynomial.Polynomial 
 
-wtCutoff = [12, 15, 21,
+w2fModel = [0, 288.798017, -81.388061, 10.101958, -0.623565, 0.018835, -0.000222]
+finModel = [0, 210.431209, -65.244076, 9.294793, -0.678313, 0.024900, -0.000365]
+awgModel = [0.43503557, 2.16250341, -0.09743488, 0.00122924]
+awfiModel = [0.90582088, 1.59691733, 0.24820408, -0.01655183, 0.00028117]
+awfcModel = [1.1, 0.10728206]
+awgAdjust = [273, 0, 23.1]
+awfcAdjust = [2.57, 0, 23.1]
+
+wtCutoff = [12, 20, 30,
             50, 80, 120, 160,
             200, 225, 245, 265]
 
 priceCutoff = [0.29721160, 0.19111814, 0.11239397,
                0.09492490, 0.08964955, 0.08389767, 0.08180934,
                0.08025336, 0.07925855, 0.09876173, 0.10228566]
+
+priceCutoff2 = [0.31444049, 0.21165624, 0.13395160,
+				0.09024673, 0.09004769, 0.08920827, 0.08843443,
+				0.08818230, 0.08798901, 0.09244352, 0.09587957]
 
 ##sm = SalesModel(MarketSize = 170, CarcassAvg = 229, CarcassStdDev = 14, LeanAvg = 54.2,
 ##                LeanStdDev = 2.17, YieldAvg = 76.2, BasePrice = 80)
@@ -47,11 +53,23 @@ priceCutoff = [0.29721160, 0.19111814, 0.11239397,
 ##plt.plot(x,y)
 ##plt.show()
 
-gm = PigGrowthModel(awgModel, awfcModel, w2fModel, 12, awgAdjust, fccumAdjust, priceCutoff, wtCutoff)
+gm = PigGrowthModel(awgModel, awfcModel, awgAdjust, awfcAdjust, priceCutoff, wtCutoff)
+bm = BarnModel(w2fModel, gm, BarnSize = 170)
 
-# x = numpy.arange(0, 40.1, .1)
-# plt.plot(x, gm.awfc.model(x))
-# plt.plot(x, gm.awfc_shift.model(x))
+print gm.aw_feed_cost.integrate(0, 23.1)
+print bm.aw_feed_cost.integrate(0, 23.1)
+print bm.calc_feed_cost_diff(270, 290, "wt")
+
+# x = numpy.arange(0, 24.6, .1)
+# plt.plot(x, bm.aw_feed_cost.model(x))
 # plt.show()
 
-print gm.feed_cost_total.integrate(0,26)
+# print gm.fc_cum.model(23.1)
+# print gm.fi_cum.model(23.1) / gm.g_cum.model(23.1)
+# print gm.awfc.integrate(0,23.1) / 23.1
+
+# print gm.fc_cum.model(23.1)
+# print gm.g_cum.model(23.1)
+# print gm.fi_cum.model(23.1)
+
+# print gm.aw_feed_cost.integrate(0, 23.1)
